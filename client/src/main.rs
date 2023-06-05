f26031c0e0d99ac09c90965e8b4a07b9d41432ac
Gabe C <gabe@fracturedcode.net>
2023-06-05 03:50:38 -0700

    fix memory leak and readd failure counting

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,8 +1,8 @@
-use futures::{StreamExt, TryStreamExt};
+use futures::StreamExt;
 use futures::stream::FuturesUnordered;
 use tokio::io::{self, AsyncReadExt, AsyncWriteExt};
 use tokio::net::TcpStream;
-use tokio::sync::OwnedSemaphorePermit;
+use tokio::sync::{OwnedSemaphorePermit};
 use tokio::sync::Semaphore;
 use tokio::task;
 use std::sync::Arc;
@@ -28,23 +28,30 @@ async fn send_request(msg: &[u8], permit: OwnedSemaphorePermit) -> io::Result<()
 async fn main() -> io::Result<()> {
 	let msg =
 		b"GET / HTTP/1.1\r\nHost: localhost:5000\r\nUser-Agent: CustomRustClient/0.1.0\r\nUseless body info Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce euismod risus eget turpis eleifend\r\n\r\n";
-	
-	let tasks: FuturesUnordered<task::JoinHandle<Result<(), io::Error>>> = FuturesUnordered::new();
+	let simultaneity: usize = 10000;
+
+	let mut tasks: FuturesUnordered<task::JoinHandle<Result<(), io::Error>>> = FuturesUnordered::new();
 	let (mut requests, mut failures) = (0, 0);
-	let semaphore = Arc::new(Semaphore::new(10000));
+	let semaphore = Arc::new(Semaphore::new(simultaneity));
 	loop {
-		let permit = semaphore.clone().acquire_owned().await.unwrap();
+		let completed_tasks: i32  = tasks.len() as i32 - simultaneity as i32;
+		if completed_tasks > 0 {
+			for _ in 0..completed_tasks {
+				match tasks.next().await {
+					Some(Err(_)) => failures += 1,
+					_ => (),
+				}
+			}
+		}
 		
+		let permit = semaphore.clone().acquire_owned().await.unwrap();
 		let task = task::spawn(send_request(msg, permit));
 		tasks.push(task);
 		
-		//Swhile let Ok(_) = tasks.next().await.unwrap()
-
-
-		//if let Err(_) = send_request(msg).await {
-		//	failures += 1;
-		//}
 		requests += 1;
-		print!("\rRequests: {}, Failures: {}", requests, failures);
+
+		if requests % 10000 == 0 {
+			print!("\rTotal requests: {}, Failures: {}", requests, failures);
+		}
 	}
 }
